e83d092921a2b144a6f257866c426d0c1dead385
vatsal-parmar <vatsalparmar33@gmail.com>
2021-12-26 09:49:11 +0530

    linkedList

---
+++ doublyLinkedList.js
@@ -0,0 +1,2 @@
+// Almost identical to Singly Linked Lists, except every node has another pointer,to the
+// previous node!
---
+++ linkedList.js
@@ -0,0 +1,142 @@
+// What is linked list?
+// A data structure that contains a head, tail and length property.
+// Linked list consists of nodes, and each nodes has a value and a pointer to another node or
+// null.
+
+// Comparisons with Arrays:
+// Lists:
+// Do not have indexes!
+// Connected via nodes with a next pointer.
+// Random access is not allowed.
+// Arrays:
+// Indexed in order!
+// Insertion and deletion can be expensive.
+// Can quickly be accessed at a specific index.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unShift(value) {
+    if (!this.head) return this.push(value);
+    let newNode = new Node(value);
+    let currentHead = this.head;
+    newNode.next = currentHead;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, value) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.value = value;
+      return true;
+    }
+    return false;
+  }
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(value);
+    if (index === 0) return !!this.unShift(value);
+
+    let newNode = new Node(value);
+    let prevNode = this.get(index - 1);
+    let tempNode = prevNode.next;
+    newNode.next = tempNode;
+    prevNode.next = newNode;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    let prevNode = this.get(index - 1);
+    let removedNode = prevNode.next;
+    prevNode.next = removedNode.next;
+    this.length--;
+    return removedNode;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next = null;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  values() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    return arr;
+  }
+}
