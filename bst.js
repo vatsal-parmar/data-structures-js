45a4496862aae153500a68ee9cd55699270725ed
vatsal-parmar <vatsalparmar33@gmail.com>
2022-01-30 09:47:40 +0530

    bfs & dfs

--- bst.js
+++ bst.js
@@ -53,4 +53,51 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+  bfs() {
+    let data = [],
+      queue = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  dfsPreOrder() {
+    let data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      data.push(node);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+
+    return data;
+  }
+  dfsPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node);
+    }
+    traverse(this.root);
+    return data;
+  }
+  dfsInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
